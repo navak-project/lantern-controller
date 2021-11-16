246a2ff8d3f66cf1a42ccaf43445c57a172ce241
netherwaves <avery@netherwaves.com>
2021-11-16 16:12:18 -0500

    add comments in multi_file_playback and teensy_wire_test

--- Teensy/teensy_wire_test/teensy_wire_test.ino
+++ Teensy/teensy_wire_test/teensy_wire_test.ino
@@ -15,17 +15,23 @@ void loop() {
 }
 
 void receiveOSC(int a) {
+  // define OSC bundle
   OSCBundle bundleIN;
 
+  // decode incoming OSC message
   while (Wire1.available()) {
     bundleIN.fill(Wire1.read());
   }
 
+  // list of callbacks
   if (!bundleIN.hasError()) {
     bundleIN.dispatch("/audio/test", audioTest);
+    // bundleIN.dispatch("/audio/test2", audioTest2);
+    // bundleIN.dispatch("/audio/test3", audioTest3);
   }
 }
 
+// callback test (prints out first msg argument)
 void audioTest(OSCMessage &msg) {
   Serial.println(msg.getInt(0));
 }

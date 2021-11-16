5430d792e8d7e92073c3e7a05e0fa6a1884cc35e
netherwaves <avery@netherwaves.com>
2021-11-16 16:10:25 -0500

    add boilerplate code for Wire communication to Teensy

---
+++ Arduino/LanternMain/Lantern_OSCWire.ino
@@ -0,0 +1,18 @@
+void InitWire() {
+    Wire.begin();
+}
+
+void AudioTest() {
+    // construct message
+    OSCMessage msg("/audio/test");
+    msg.add(1);
+    // send
+    SendToTeensy(&msg);
+}
+
+// through I2C communication
+void SendToTeensy(OSCMessage &msg) {
+    Wire.beginTransmission(0x2D);
+    msg.send(Wire);
+    Wire.endTransmission();
+}

806493067788f3b37615c94999fa32892d1b6ef7
netherwaves <avery@netherwaves.com>
2021-11-28 23:04:50 -0500

    create first version of heartbeat sound

--- Teensy/TeensyMain/HeartbeatEvents.ino
+++ Teensy/TeensyMain/HeartbeatEvents.ino
@@ -2,19 +2,84 @@ int heartRateBPM;
 unsigned long heartRateMillis;
 
 bool heartbeatStarted = false;
+bool isHeartNotePlaying = false;
 elapsedMillis heartbeatTimer;
 
 // NICE TO HAVE: occasional (40% chance) intermediate random-offset pulse
 
 void initHeartbeat() {
-  setHeartRate(100);
+  // set initial heart rate
+  // TODO: remove this and manage from ESP32 messaging instead
+  setHeartRate(70);
 
-  // initialize instruments
-  hbSynth1.setInstrument(flute_01);
+
+  // configure WT instruments
+  hbSynth1.setInstrument(hb_part1);
   hbSynth1.amplitude(1);
-  
+  hbSynth2.setInstrument(hb_part2);
+  hbSynth2.amplitude(1);
+  hbSynth3.setInstrument(hb_part3);
+  hbSynth3.amplitude(1);
+  hbSynth4.setInstrument(hb_part4);
+  hbSynth4.amplitude(1);
+
+
+  // configure WT mixer
+  hbWavetableMixer.gain(0, 1);
+  hbWavetableMixer.gain(1, 1);
+  hbWavetableMixer.gain(2, 1);
+  hbWavetableMixer.gain(3, 1);
+
+
+  // configure LFO parameters
+  sineMod1.frequency(0.08);
+  sineMod1.amplitude(0.5);
+  sineMod1.offset(0.5);
+
+  sineMod2.frequency(0.13);
+  sineMod2.amplitude(0.5);
+  sineMod2.offset(0.5);
+
+  sineMod3.frequency(0.11);
+  sineMod3.amplitude(0.5);
+  sineMod3.offset(0.5);
+
+
+  // configure RM network
+  sineRing1.frequency(5600);
+  sineRing1.amplitude(1);
+  sineRing1.frequencyModulation(0.07);    // [5600 - 6000]
+
+  sineRing2.frequency(2000);
+  sineRing2.amplitude(1);
+  sineRing2.frequencyModulation(0.1);     // [2000 - 2200]
+
+  sineRing3.frequency(140);
+  sineRing3.amplitude(1);
+  sineRing3.frequencyModulation(0.15);    // [140 - 161]
+
+
+  // configure static mixer gains
+  hbStaticMixer.gain(0, 0.125);
+  hbStaticMixer.gain(1, 0.25);
+  hbStaticMixer.gain(2, 0.5);
+  hbStaticMixer.gain(3, 0.125);
+
+
+  // configure static filter
+  staticFilter.setLowpass(0, 4000, 0.3);
+
+
+  // mix pure and static signals
+  // TODO: react to tree area enter/leave events
   hbMixer.gain(0, 1);
-  mainMixer.gain(2, 0.5);
+  hbMixer.gain(1, 1);
+  mainMixer.gain(2, 1);
+
+
+  // initialize faders
+  hbPureFader.fadeOut(10);
+  hbStaticFader.fadeOut(10);
 }
 
 void setHeartRate(int rate) {
@@ -32,6 +97,8 @@ void startHeartbeat() {
   // begin heartbeat loop
   heartbeatTimer = 0;
   heartbeatStarted = true;
+
+  hbStaticFader.fadeIn(2000);
 }
 
 void attenuateHeartbeat() {
@@ -46,7 +113,7 @@ void toConstantLight() {
 void fadeOutAll() {
   // turn off all vital energy instruments
   heartbeatStarted = false;
-  hbSynth1.stop();
+  stopHeartNote();
 }
 
 void updateHeartbeat() {
@@ -56,11 +123,35 @@ void updateHeartbeat() {
     heartbeatTimer = 0;
 
     // trigger instrument
-    hbSynth1.playFrequency(660);
+    // NOTE TO SELF FOR THE FUTURE:
+    // THIS IS MIDI VELOCITY!!!! NOT SIGNAL AMPLITUDE!!! LOLLL
+    int velo = random(30, 100);
+    playHeartNote(72, velo);
   }
 
-//  // add check for stopping wavetable
-  if (heartbeatTimer > heartRateMillis - 200 && hbSynth1.isPlaying()) {
-    hbSynth1.stop();
+  // play 250ms note (ie. release env after that time)
+  if (heartbeatTimer > 250 && isHeartNotePlaying) {
+    stopHeartNote();
   }
+}
+
+
+// utilities
+// TODO: optimize
+void playHeartNote(int note, int velo) {
+  hbSynth1.playNote(note, velo);
+  hbSynth2.playNote(note + 7, velo);
+  hbSynth3.playNote(note + 14, velo);
+  hbSynth4.playNote(note, velo);
+
+  isHeartNotePlaying = true;
+}
+
+void stopHeartNote() {
+  hbSynth1.stop();
+  hbSynth2.stop();
+  hbSynth3.stop();
+  hbSynth4.stop();
+
+  isHeartNotePlaying = false;
 }
--- Teensy/TeensyMain/TeensyMain.ino
+++ Teensy/TeensyMain/TeensyMain.ino
@@ -10,33 +10,77 @@
 #include <OSCBundle.h>
 
 // instruments
-#include "flute_01_samples.h"
+#include "hb_part1_samples.h"
+#include "hb_part2_samples.h"
+#include "hb_part3_samples.h"
+#include "hb_part4_samples.h"
 
 // GUItool: begin automatically generated code
-AudioPlaySdWav           lanternLoop;    //xy=378,222
-AudioSynthWavetable      hbSynth1;      //xy=380,302
-AudioEffectFade          lanternLoopFade; //xy=574,215
-AudioPlaySdWav           lanternEvents;  //xy=575,146
-AudioMixer4              hbMixer;         //xy=603,320
-AudioMixer4              mainMixer;         //xy=827,226
-AudioOutputI2S           i2sOut;           //xy=1002,225
-AudioConnection          patchCord1(lanternLoop, 0, lanternLoopFade, 0);
-AudioConnection          patchCord2(hbSynth1, 0, hbMixer, 0);
-AudioConnection          patchCord3(lanternLoopFade, 0, mainMixer, 1);
-AudioConnection          patchCord4(lanternEvents, 0, mainMixer, 0);
-AudioConnection          patchCord5(hbMixer, 0, mainMixer, 2);
-AudioConnection          patchCord6(mainMixer, 0, i2sOut, 0);
-AudioConnection          patchCord7(mainMixer, 0, i2sOut, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=459,571
+AudioSynthWavetable      hbSynth2; //xy=124.5,266
+AudioSynthWavetable      hbSynth3; //xy=125.5,307
+AudioSynthWavetable      hbSynth4;  //xy=125.5,346
+AudioSynthWavetable      hbSynth1;      //xy=126.5,224
+AudioSynthWaveform       sineMod1;      //xy=146,434
+AudioSynthWaveform       sineMod2; //xy=146,482
+AudioSynthWaveform       sineMod3; //xy=146,528
+AudioMixer4              hbWavetableMixer;         //xy=338.5,282
+AudioSynthNoiseWhite     noise1;         //xy=379,593
+AudioSynthWaveformModulated sineRing3; //xy=384,534
+AudioSynthWaveformModulated sineRing1;   //xy=386,440
+AudioSynthWaveformModulated sineRing2;  //xy=386,488
+AudioEffectMultiply      ringMod4; //xy=597,590
+AudioEffectMultiply      ringMod1;      //xy=600,441
+AudioEffectMultiply      ringMod3; //xy=600,540
+AudioEffectMultiply      ringMod2; //xy=601,490
+AudioMixer4              hbStaticMixer; //xy=794,514
+AudioEffectFade          hbPureFader;          //xy=978,294
+AudioEffectFade          hbStaticFader; //xy=978,337
+AudioFilterBiquad        staticFilter;        //xy=986,513
+AudioPlaySdWav           lanternLoop;    //xy=1030,210
+AudioMixer4              hbMixer;         //xy=1173,324
+AudioEffectFade          lanternLoopFade; //xy=1226,203
+AudioPlaySdWav           lanternEvents;  //xy=1227,134
+AudioMixer4              mainMixer;         //xy=1479,214
+AudioOutputI2S           i2sOut;           //xy=1654,213
+AudioConnection          patchCord1(hbSynth2, 0, hbWavetableMixer, 1);
+AudioConnection          patchCord2(hbSynth3, 0, hbWavetableMixer, 2);
+AudioConnection          patchCord3(hbSynth4, 0, hbWavetableMixer, 3);
+AudioConnection          patchCord4(hbSynth1, 0, hbWavetableMixer, 0);
+AudioConnection          patchCord5(sineMod1, 0, sineRing1, 0);
+AudioConnection          patchCord6(sineMod2, 0, sineRing2, 0);
+AudioConnection          patchCord7(sineMod3, 0, sineRing3, 0);
+AudioConnection          patchCord8(hbWavetableMixer, 0, ringMod1, 0);
+AudioConnection          patchCord9(hbWavetableMixer, 0, ringMod2, 0);
+AudioConnection          patchCord10(hbWavetableMixer, 0, ringMod3, 1);
+AudioConnection          patchCord11(hbWavetableMixer, 0, ringMod4, 0);
+AudioConnection          patchCord12(hbWavetableMixer, fade1);
+AudioConnection          patchCord13(noise1, 0, ringMod4, 1);
+AudioConnection          patchCord14(sineRing3, 0, ringMod3, 0);
+AudioConnection          patchCord15(sineRing1, 0, ringMod1, 1);
+AudioConnection          patchCord16(sineRing2, 0, ringMod2, 1);
+AudioConnection          patchCord17(ringMod4, 0, hbStaticMixer, 3);
+AudioConnection          patchCord18(ringMod1, 0, hbStaticMixer, 0);
+AudioConnection          patchCord19(ringMod3, 0, hbStaticMixer, 2);
+AudioConnection          patchCord20(ringMod2, 0, hbStaticMixer, 1);
+AudioConnection          patchCord21(hbStaticMixer, staticFilter);
+AudioConnection          patchCord22(fade1, 0, hbMixer, 0);
+AudioConnection          patchCord23(fade2, 0, hbMixer, 1);
+AudioConnection          patchCord24(staticFilter, fade2);
+AudioConnection          patchCord25(lanternLoop, 0, lanternLoopFade, 0);
+AudioConnection          patchCord26(hbMixer, 0, mainMixer, 2);
+AudioConnection          patchCord27(lanternLoopFade, 0, mainMixer, 1);
+AudioConnection          patchCord28(lanternEvents, 0, mainMixer, 0);
+AudioConnection          patchCord29(mainMixer, 0, i2sOut, 0);
+AudioConnection          patchCord30(mainMixer, 0, i2sOut, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=601,53
 // GUItool: end automatically generated code
+
 
 // constants
 #define WIRE_ADDR        0x2D
 #define SDCARD_CS_PIN    10
 #define SDCARD_MOSI_PIN  7
 #define SDCARD_SCK_PIN   14
-
-// properties
 
 void setup() {
   // init Teensy Audio + SD
@@ -54,7 +98,7 @@ void setup() {
 
 void loop() {
   // for monitoring audio memory usage; uncomment if necessary
-  monitorBlocks();
+  //monitorBlocks();
 
   // lantern loop manager
   updateLanternLoop();
